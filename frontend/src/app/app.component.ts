d8acbdf5c3db17491fae2cfee6853bed06b178ec
Maciej Urbas <maciej.urbas3@gmail.com>
2022-11-09 02:18:58 +0100

    :sparkles: Add `show` button while window scroll

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'frontend';
+
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll() {
+    let navlogoHeightValue;
+
+    const navlogoHeight = document.querySelector('.nav-logo');
+    navlogoHeightValue = navlogoHeight?.clientHeight;
+
+    // console.log(navlogoHeightValue);
+
+    if (navlogoHeightValue != undefined) {
+      console.log(navlogoHeightValue);
+      if (window.scrollY >= navlogoHeightValue - 10) {
+        document
+          .querySelector('.scroll-to-top_button')
+          ?.classList.add('show-scroll-to-top_button');
+      } else {
+        document
+          .querySelector('.scroll-to-top_button')
+          ?.classList.remove('show-scroll-to-top_button');
+      }
+    }
+
+    // return 0;
+  }
 }
